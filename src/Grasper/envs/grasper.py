8c372bda589b612deb463f42c269092396723b25
Will Sumerfield <will.sumerfield24@imperial.ac.uk>
2025-02-10 09:43:20 +0000

    Simple manual control implemented

--- src/Grasper/envs/grasper.py
+++ src/Grasper/envs/grasper.py
@@ -5,43 +5,40 @@ import pygame
 import numpy as np
 
 
-class Actions(Enum):
+class MoveActions(Enum):
     right = 0
     up = 1
     left = 2
     down = 3
 
+class ClawActions(Enum):
+    open = 0
+    close = 1
 
 class GrasperEnv(gym.Env):
-    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 10}
 
-    def __init__(self, render_mode=None, size=5):
-        self.size = size  # The size of the square grid
-        self.window_size = 512  # The size of the PyGame window
-
-        # Observations are dictionaries with the agent's and the target's location.
-        # Each location is encoded as an element of {0, ..., `size`}^2,
-        # i.e. MultiDiscrete([size, size]).
+    def __init__(self, render_mode=None):
+        self.window_size = (768, 512)  # The size of the PyGame window
+        
+        # What the agent sees
         self.observation_space = spaces.Dict(
             {
-                "agent": spaces.Box(0, size - 1, shape=(2,), dtype=int),
-                "target": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "agent":  spaces.Box(0, self.window_size[0]-1, shape=(2,), dtype=float), # Agent Location
+                "ball":   spaces.Box(0, self.window_size[0]-1, shape=(2,), dtype=float), # Ball Location
+                "target": spaces.Box(0, self.window_size[0]-1, shape=(2,), dtype=float), # Target Location
+                "claw_angle": spaces.Box(0, 180, shape=(1,), dtype=float),
             }
         )
 
-        # We have 4 actions, corresponding to "right", "up", "left", "down", "right"
-        self.action_space = spaces.Discrete(4)
+        # What the agent can do
+        self.action_space = spaces.MultiDiscrete([4, 2]) # 4 movement, 2 claw
 
-        """
-        The following dictionary maps abstract actions from `self.action_space` to 
-        the direction we will walk in if that action is taken.
-        i.e. 0 corresponds to "right", 1 to "up" etc.
-        """
         self._action_to_direction = {
-            Actions.right.value: np.array([1, 0]),
-            Actions.up.value: np.array([0, 1]),
-            Actions.left.value: np.array([-1, 0]),
-            Actions.down.value: np.array([0, -1]),
+            MoveActions.right.value: np.array([1,  0]),
+            MoveActions.up.value:    np.array([0,  1]),
+            MoveActions.left.value:  np.array([-1, 0]),
+            MoveActions.down.value:  np.array([0, -1]),
         }
 
         assert render_mode is None or render_mode in self.metadata["render_modes"]
@@ -58,28 +55,25 @@ class GrasperEnv(gym.Env):
         self.clock = None
 
     def _get_obs(self):
-        return {"agent": self._agent_location, "target": self._target_location}
+        return {"agent": self._agent_location, "ball": np.array([1.0,0]),
+                "target": self._target_location, "claw_angle": np.array([1.0,0])}
 
     def _get_info(self):
-        return {
-            "distance": np.linalg.norm(
-                self._agent_location - self._target_location, ord=1
-            )
-        }
+        return {}
 
     def reset(self, seed=None, options=None):
         # We need the following line to seed self.np_random
         super().reset(seed=seed)
 
         # Choose the agent's location uniformly at random
-        self._agent_location = self.np_random.integers(0, self.size, size=2, dtype=int)
+        self._agent_location = self.window_size[0] * self.np_random.random(2, dtype=float)
 
         # We will sample the target's location randomly until it does not
         # coincide with the agent's location
         self._target_location = self._agent_location
         while np.array_equal(self._target_location, self._agent_location):
             self._target_location = self.np_random.integers(
-                0, self.size, size=2, dtype=int
+                0, self.window_size[0], size=2, dtype=int
             )
 
         observation = self._get_obs()
@@ -92,10 +86,11 @@ class GrasperEnv(gym.Env):
 
     def step(self, action):
         # Map the action (element of {0,1,2,3}) to the direction we walk in
-        direction = self._action_to_direction[action]
+        direction = self._action_to_direction[action[0]]
+
         # We use `np.clip` to make sure we don't leave the grid
         self._agent_location = np.clip(
-            self._agent_location + direction, 0, self.size - 1
+            self._agent_location + direction, 0, self.window_size[0] - 1
         )
         # An episode is done iff the agent has reached the target
         terminated = np.array_equal(self._agent_location, self._target_location)
@@ -116,49 +111,26 @@ class GrasperEnv(gym.Env):
         if self.window is None and self.render_mode == "human":
             pygame.init()
             pygame.display.init()
-            self.window = pygame.display.set_mode((self.window_size, self.window_size))
+            self.window = pygame.display.set_mode((self.window_size[0], self.window_size[1]))
         if self.clock is None and self.render_mode == "human":
             self.clock = pygame.time.Clock()
 
-        canvas = pygame.Surface((self.window_size, self.window_size))
+        canvas = pygame.Surface((self.window_size[0], self.window_size[1]))
         canvas.fill((255, 255, 255))
-        pix_square_size = (
-            self.window_size / self.size
-        )  # The size of a single grid square in pixels
 
         # First we draw the target
-        pygame.draw.rect(
-            canvas,
-            (255, 0, 0),
-            pygame.Rect(
-                pix_square_size * self._target_location,
-                (pix_square_size, pix_square_size),
-            ),
-        )
+        pygame.draw.circle(canvas,
+                           (255, 0, 0),
+                           self._target_location,
+                           10)
+
         # Now we draw the agent
         pygame.draw.circle(
             canvas,
             (0, 0, 255),
-            (self._agent_location + 0.5) * pix_square_size,
-            pix_square_size / 3,
+            self._agent_location,
+            10,
         )
-
-        # Finally, add some gridlines
-        for x in range(self.size + 1):
-            pygame.draw.line(
-                canvas,
-                0,
-                (0, pix_square_size * x),
-                (self.window_size, pix_square_size * x),
-                width=3,
-            )
-            pygame.draw.line(
-                canvas,
-                0,
-                (pix_square_size * x, 0),
-                (pix_square_size * x, self.window_size),
-                width=3,
-            )
 
         if self.render_mode == "human":
             # The following line copies our drawings from `canvas` to the visible window
--- src/manual_control.py
+++ src/manual_control.py
@@ -1,6 +1,105 @@
 """
-Manually play in the environment using the keyboard.
+Play the game manually using the keyboard
 """
 
+import time
+
+import gymnasium as gym
+import numpy as np
+from pynput import keyboard
+
+import Grasper
+
+
+FPS = 1/60.0
+
+key_presses = {
+    "w": False,
+    "a": False,
+    "s": False,
+    "d": False,
+    "space": False,
+    "esc": False
+}
+
+
 def manual_control():
-    pass
+    global key_presses
+
+    env = gym.make('Grasper/Grasper-v0', render_mode="human")
+    env.reset()
+
+    # Start a separate pynput listener thread
+    listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+    listener.start()
+
+    # Repeat until the env is complete
+    while True:
+        time.sleep(FPS)
+
+        # Quit the game
+        if key_presses["esc"]:
+            break
+
+        # Handle player input
+        movement = 0 # Default to 'up'
+        if key_presses["s"]:
+            movement = 1
+        if key_presses["a"]:
+            movement = 2
+        if key_presses["d"]:
+            movement = 3
+
+        claw = 0 # Default to opening the claw
+        if key_presses["space"]:
+            claw = 1
+
+        # Take a step in the environment
+        obs, reward, done, _, info = env.step(np.array([movement, claw]))
+        env.render()
+        if done:
+            env.reset()
+
+    env.close()
+
+
+def on_press(key):
+        global key_presses
+
+        try:
+            # Player inputs
+            if key.char == "w":
+                key_presses["w"] = True
+            if key.char == "s":
+                key_presses["s"] = True
+            if key.char == "a":
+                key_presses["a"] = True
+            if key.char == "d":
+                key_presses["d"] = True
+        
+        except AttributeError:
+            if key == keyboard.Key.space:
+                key_presses["space"] = True
+            if key == keyboard.Key.esc:
+                key_presses["esc"] = True
+
+
+def on_release(key):
+        global key_presses
+
+        try:
+            # Player inputs
+            if key.char == "w":
+                key_presses["w"] = False
+            if key.char == "s":
+                key_presses["s"] = False
+            if key.char == "a":
+                key_presses["a"] = False
+            if key.char == "d":
+                key_presses["d"] = False
+        
+        except AttributeError:
+            if key == keyboard.Key.space:
+                key_presses["space"] = False
+            if key == keyboard.Key.esc:
+                key_presses["esc"] = False
